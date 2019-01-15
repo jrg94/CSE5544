060b1e480ceb0fbf8ad2ae168858eade5ef94654
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-01-15 16:08:42 -0500

    Read CSV as list of dicts

--- assignment01/src/vtk/main.py
+++ assignment01/src/vtk/main.py
@@ -1,43 +1,19 @@
 import vtk
+import csv
+
+
+def get_csv_as_dict_list():
+    dict_list = []
+    with open('../../data/EHRdataSample.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        for line in csv_reader:
+            dict_list.append(line)
+    return dict_list
+
 
 def main():
+    dict_list = get_csv_as_dict_list()
     colors = vtk.vtkNamedColors()
-
-    # Create a pentagon
-    polygonSource = vtk.vtkRegularPolygonSource()
-    polygonSource.SetNumberOfSides(5)
-    polygonSource.SetRadius(5)
-    polygonSource.SetCenter(0, 0, 0)
-
-    shrink = vtk.vtkShrinkPolyData()
-    shrink.SetInputConnection(polygonSource.GetOutputPort())
-    shrink.SetShrinkFactor(.9)
-
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(shrink.GetOutputPort())
-
-    back = vtk.vtkProperty()
-    back.SetColor(colors.GetColor3d("Tomato"))
-
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().EdgeVisibilityOn()
-    actor.GetProperty().SetLineWidth(5)
-    actor.GetProperty().SetColor(colors.GetColor3d("Banana"))
-    actor.SetBackfaceProperty(back)
-
-    renderer = vtk.vtkRenderer()
-    renderWindow = vtk.vtkRenderWindow()
-    renderWindow.SetWindowName("Regular Polygon Source")
-    renderWindow.AddRenderer(renderer)
-    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-    renderWindowInteractor.SetRenderWindow(renderWindow)
-
-    renderer.AddActor(actor)
-    renderer.SetBackground(colors.GetColor3d("Silver"))
-
-    renderWindow.Render()
-    renderWindowInteractor.Start()
 
 
 if __name__ == '__main__':
