60f22a70d1f64ed10015d3ca0303eabc48b0afc5
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-01-16 00:07:47 -0500

    Added sorting

--- assignment01/src/vtk/main.py
+++ assignment01/src/vtk/main.py
@@ -19,6 +19,7 @@ def screen_shot(window, file_name):
     :param file_name: a file name without the extension
     :return: None
     """
+    window.Render()
     w2if = vtk.vtkWindowToImageFilter()
     w2if.SetInput(window)
     w2if.Update()
@@ -29,11 +30,7 @@ def screen_shot(window, file_name):
     writer.Write()
 
 
-def scatter_plot(dict_list, key, pic_name):
-    view = vtk.vtkContextView()
-    view.GetRenderer().SetBackground(1.0, 1.0, 1.0)
-    view.GetRenderWindow().SetSize(800, 800)
-
+def scatter_plot(view, dict_list, key_y):
     chart = vtk.vtkChartXY()
     view.GetScene().AddItem(chart)
     chart.SetShowLegend(True)
@@ -41,46 +38,55 @@ def scatter_plot(dict_list, key, pic_name):
     table = vtk.vtkTable()
 
     # Create x-axis collection
-    arrX = vtk.vtkFloatArray()
-    arrX.SetName('X Axis')
+    arrX = vtk.vtkIntArray()
+    arrX.SetName("X-axis")
+    chart.GetAxis(vtk.vtkAxis.BOTTOM).SetTitle("Index")
 
     # Create cosine collection
     arrC = vtk.vtkFloatArray()
-    arrC.SetName('Age')
+    arrC.SetName(key_y)
+    chart.GetAxis(vtk.vtkAxis.LEFT).SetTitle(key_y)
 
     # Add columns to table
     table.AddColumn(arrX)
     table.AddColumn(arrC)
+
+    # Sort dict_list
+    dict_list = sorted(dict_list, key=lambda data: float(data[key_y]))
 
     # Populate columns
     numPoints = len(dict_list)
     table.SetNumberOfRows(numPoints)
     for i in range(numPoints):
         table.SetValue(i, 0, i)
-        table.SetValue(i, 1, dict_list[i][key])
+        table.SetValue(i, 1, dict_list[i][key_y])
+
+    print(dict_list)
 
     # Key
     points = chart.AddPlot(vtk.vtkChart.POINTS)
     points.SetInputData(table, 0, 1)
     points.SetColor(0, 100, 0, 255)
     points.SetWidth(1.0)
-    points.SetMarkerStyle(vtk.vtkPlotPoints.CROSS)
+    points.SetMarkerStyle(vtk.vtkPlotPoints.CIRCLE)
 
     view.GetRenderWindow().SetMultiSamples(0)
-    view.GetRenderWindow().Render()
-
-    # Screen shot
-    screen_shot(view.GetRenderWindow(), pic_name)
-
-    view.GetInteractor().Initialize()
-    view.GetInteractor().Start()
 
 
 def main():
     dict_list = get_csv_as_dict_list()
-    scatter_plot(dict_list, "Age", "age-by-index-scatter-plot")
-    #scatter_plot(dict_list, "Age_TBI")
 
+    view = vtk.vtkContextView()
+    view.GetRenderer().SetBackground(1.0, 1.0, 1.0)
+    view.GetRenderWindow().SetSize(800, 800)
+
+    scatter_plot(view, dict_list, "Age")
+
+    # Screen shot
+    screen_shot(view.GetRenderWindow(), "age-by-index-scatter-plot-sorted")
+
+    view.GetInteractor().Initialize()
+    view.GetInteractor().Start()
 
 
 if __name__ == '__main__':
