6c3a7d465ab92b41ce57a6998fc701fd0eca139e
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-03 15:22:07 -0500

    Removed spaces from csv output

--- assignment04/src/data-processing.py
+++ assignment04/src/data-processing.py
@@ -1,4 +1,4 @@
 with open("E:\\Projects\\CSE5544\\assignment04\\data\\testGHZ400.data") as f:
     with open("E:\\Projects\\CSE5544\\assignment04\\data\\testGHZ400clean.data", "w") as out:
         for line in f:
-            print(", ".join(line.split()), file=out)
+            print(",".join(line.split()), file=out)
--- assignment04/src/graph.js
+++ assignment04/src/graph.js
@@ -34,17 +34,6 @@ d3.csv("data/testGHZ400clean.data").then(function(data) {
     p.px /= magnitude;
     p.py /= magnitude;
 
-    // vector
-    svg.append("g")
-    .append("path")
-    .attr("d", "M" + xScale(0) + " " + yScale(0) + " L" + xScale(p.px) + " " + yScale(p.py))
-    .attr("stroke", "blue")
-    .attr("stroke-width", 1)
-    .attr("fill", "none")
-    .attr("transform", "translate(" + (xScale(p.x) - xScale(0)) + "," + (yScale(p.y) - yScale(0)) + ")")
-    ;
-
-
   });
 
 });
