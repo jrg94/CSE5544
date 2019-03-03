ee5e776173b913d7385644fd724426c33a905db1
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-03 14:36:32 -0500

    Played with normalization

--- assignment04/src/graph.js
+++ assignment04/src/graph.js
@@ -1,46 +1,50 @@
+var margin = 100;
+var width = 800;
+var height = 800;
+
 d3.select("body").append("svg")
-  .attr("width", 960)
-  .attr("height", 800);
+  .attr("width", width + 2 * margin)
+  .attr("height", height + 2 * margin)
+  .append("g")
+  .attr("translate(" + margin + "," + margin + ")");
 
 var svg = d3.select("svg");
 
-var margin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
-}
-
-var width = +svg.attr("width") - margin.left - margin.right
-var height = +svg.attr("height") - margin.top - margin.bottom
-
-var g = svg.append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-svg.append("text")
-  .attr("x", width / 2)
-  .attr("y", 20)
-  .attr("text-anchor", "middle")
-  .style("font-size", "16px")
-  .style("text-decoration", "underline")
-  .text("Patient ID vs. Days from First TBI");
-
-var parseTime = d3.timeParse("%d-%b-%y");
-
-var yScale = d3.scaleLinear().range([height,0]).domain([-5,5]);
-var xScale = d3.scaleLinear().range([0,width]).domain([-5,5]);
+var yScale = d3.scaleLinear().range([height, 0]).domain([-5, 5]);
+var xScale = d3.scaleLinear().range([0, width]).domain([-5, 5]);
 
 svg.append("g")
-   .attr("transform", "translate(0," + height/2 + ")")
-   .call(d3.axisBottom(xScale));
+  .attr("transform", "translate(0," + height / 2 + ")")
+  .call(d3.axisBottom(xScale));
 
 svg.append("g")
-   .attr("transform", "translate(" + width/2 + ",0)")
-   .call(d3.axisLeft(yScale));
+  .attr("transform", "translate(" + width / 2 + ",0)")
+  .call(d3.axisLeft(yScale));
+
+
 
 d3.csv("data/testGHZ400clean.data").then(function(data) {
 
   console.log(data);
 
+  data.forEach(function(p) {
+    magnitude = Math.sqrt(p.x * p.x + p.y * p.y);
+
+    // Normalize
+    p.px /= magnitude;
+    p.py /= magnitude;
+
+    // vector
+    svg.append("g")
+    .append("path")
+    .attr("d", "M" + xScale(0) + " " + yScale(0) + " L" + xScale(p.px) + " " + yScale(p.py))
+    .attr("stroke", "blue")
+    .attr("stroke-width", 1)
+    .attr("fill", "none")
+    .attr("transform", "translate(" + (xScale(p.x) - xScale(0)) + "," + (yScale(p.y) - yScale(0)) + ")")
+    ;
+
+
+  });
 
 });
